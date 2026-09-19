185046dc48b503905e51a46070372b3438d18f3b
Pookachu <mc.astradal@gmail.com>
2026-09-19 10:11:34 -0500

    fmt 1

--- src/game_systems/src/lib.rs
+++ src/game_systems/src/lib.rs
@@ -94,7 +94,7 @@ fn register_tick_systems(schedule: &mut Schedule) {
             .chain()
             .before(TickPhase::VisibleTracking),
     );
-    
+
     schedule.add_systems(player::player_despawn::handle);
     schedule.add_systems(player::player_join_message::handle);
     schedule.add_systems(player::player_leave_message::handle);
